06da6fda2b5f95801d7bcaa6640a2c26c1ff0bb4
ArenisMiranda <mirandaarenis@gmail.com>
2022-05-20 00:16:14 -0500

    cambio de nombre de bd

--- models/conexion.php
+++ models/conexion.php
@@ -10,7 +10,7 @@ $conn = mysqli_connect(
 class Database{
     static public function conectar(){
         try{
-            $pdo = new PDO("mysql:host=localhost;port=3306;dbname=conferencias", "root" , "1423");
+            $pdo = new PDO("mysql:host=localhost;port=3306;dbname=conveni2_eventos", "conveni2_vinculacion" , "JA^*Ui!&SSr#");
             $pdo->setAttribute( PDO::ATTR_EMULATE_PREPARES, false );
             $pdo -> exec("set names utf8");
             
