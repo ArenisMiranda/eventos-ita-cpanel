2475833daf455f8f16feed5419a96d4ea7089b73
ArenisMiranda <mirandaarenis@gmail.com>
2022-05-31 10:54:28 -0500

    conexion

--- models/conexion.php
+++ models/conexion.php
@@ -10,8 +10,8 @@ $conn = mysqli_connect(
 class Database{
     static public function conectar(){
         try{
-           //$pdo = new PDO("mysql:host=localhost;port=3306;dbname=conveni2_eventos", "conveni2_vinculacion" , "JA^*Ui!&SSr#");
-            $pdo = new PDO("mysql:host=localhost;port=3306;dbname=conferencias", "root" , "1423");
+           $pdo = new PDO("mysql:host=localhost;port=3306;dbname=conveni2_eventos", "conveni2_vinculacion" , "JA^*Ui!&SSr#");
+           // $pdo = new PDO("mysql:host=localhost;port=3306;dbname=conferencias", "root" , "1423");
             $pdo->setAttribute( PDO::ATTR_EMULATE_PREPARES, false );
             $pdo -> exec("set names utf8");
             
