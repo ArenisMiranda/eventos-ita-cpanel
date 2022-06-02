f1f5f203c4f276eae3f7aadb50ebb04e3c43ca25
ArenisMiranda <mirandaarenis@gmail.com>
2022-06-02 12:11:09 -0500

    navar

--- views/includes/navbar.php
+++ views/includes/navbar.php
@@ -12,30 +12,45 @@
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item dropdown">
 
-                <a class="nav-link active dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Registro </a>
-                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="nav-link active dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Registro </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
 
-                    <li class="dropdown-submenu"><a class="dropdown-item dropdown-toggle" href="#">Eventos</a>
+                        <li class="dropdown-submenu"><a class="dropdown-item dropdown-toggle" href="#">Eventos</a>
 
-                        <ul class="dropdown-menu">
+                            <ul class="dropdown-menu">
 
-                            <li><a class="dropdown-item" href="registroCursoAlumno">Alumno</a></li>
+                                <li><a class="dropdown-item" href="registroCursoAlumno">Alumno</a></li>
 
-                            <li><a class="dropdown-item" href="registroCursoDocente">Egresados y Docente</a></li>
+                                <li><a class="dropdown-item" href="registroCursoDocente">Egresados y Docente</a></li>
 
-                            <li><a class="dropdown-item" href="registroCurso">Público en general</a></li>
+                                <li><a class="dropdown-item" href="registroCurso">Público en general</a></li>
 
-                        </ul>
+                            </ul>
 
-                    </li>
+                        </li>
 
-                </ul>
+                    </ul>
 
-            </li>
-               
-                <li class="nav-item nav-right">
+                </li>
+
+                <!-- <li class="nav-item nav-right">
 
                     <a class="nav-link active" href="loginAdministrador">
+                        Login </a>
+
+                </li> -->
+
+            </ul>
+
+        </div>
+
+        <div class="d-flex">
+
+            <ul class="navbar-nav">
+
+                <li class="nav-item">
+
+                <a class="nav-link active" href="loginAdministrador">
                         Login </a>
 
                 </li>
@@ -47,3 +62,66 @@
     </div>
 
 </nav>
+
+<!-- <nav class="navbar navbar-expand-lg navbar-dark nav-color-tecnm">
+    <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand" href="inicio"><i class="bi bi-house-door-fill"></i></a>
+
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+                <li class="nav-item dropdown">
+
+                    <a class="nav-link active dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+
+                        Alumnos
+                    </a>
+
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+
+                        <li><a class="dropdown-item" href="inicioEstudiante">Iniciar Sesión</a></li>
+
+                        <li>
+
+                            <hr class="dropdown-divider" />
+
+                        </li>
+
+                        <li><a class="dropdown-item" href="registroEstudiante">Registrarse</a></li>
+
+                    </ul>
+
+                </li>
+
+                <li class="nav-item">
+
+                    <a class="nav-link active" href="inicioDocente">Docentes</a>
+
+                </li>
+
+            </ul>
+
+        </div>
+
+        <div class="d-flex">
+
+            <ul class="navbar-nav">
+
+                <li class="nav-item">
+
+                <a class="nav-link active" href="loginAdministrador">
+                        Login </a>
+
+                </li>
+
+            </ul>
+
+        </div>
+
+    </div>
+
+</nav> -->
